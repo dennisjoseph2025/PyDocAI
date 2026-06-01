a24e1a598ee74c1f5a9fc8e0f2fdde51bdefe2dd
Ubuntu <ubuntu@ip-172-31-36-216.ec2.internal>
2026-06-01 11:04:43 +0000

    fix: use STORAGES dict for Django 5.1+ S3 static/media storage

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -55,9 +55,16 @@ AWS_QUERYSTRING_AUTH = False
 AWS_S3_VERIFY = True
 
 if AWS_STORAGE_BUCKET_NAME:
+    STORAGES = {
+        "default": {
+            "BACKEND": "config.storage_backends.MediaStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "storages.backends.s3.S3StaticStorage",
+        },
+    }
     STATICFILES_STORAGE = 'storages.backends.s3.S3StaticStorage'
     DEFAULT_FILE_STORAGE = 'config.storage_backends.MediaStorage'
-
     cloudfront_domain = config('AWS_CLOUDFRONT_DOMAIN', default=None)
     if cloudfront_domain:
         STATIC_URL = f'https://{cloudfront_domain}/static/'
