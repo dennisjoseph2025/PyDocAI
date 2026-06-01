51aa30f218ad9d303b66124a55a6f4dc1871db4a
dennisjoseph2025-dotcom <dennisjoseph2025@gmail.com>
2026-06-01 14:02:27 +0530

    feat: add S3 static/media support (django-storages + boto3)

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -44,6 +44,28 @@ REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {
     'user': '1000/hour',
 }
 
+# ── S3 STATIC & MEDIA FILES ──────────────────────────────────
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default=None)
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default=None)
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default=None)
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_VERIFY = True
+
+if AWS_STORAGE_BUCKET_NAME:
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    cloudfront_domain = config('AWS_CLOUDFRONT_DOMAIN', default=None)
+    if cloudfront_domain:
+        STATIC_URL = f'https://{cloudfront_domain}/'
+        MEDIA_URL = f'https://{cloudfront_domain}/'
+    else:
+        STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+        MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
